ceeec7580d9a316263f02401ee0171e7fa653baa
Vaidas Bagdonas <vaidas.bagdonas@comsave.com>
2020-04-22 12:38:07 +0200

    init tests

---
+++ tests/Integration/PushTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Comsave\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class PushTest extends TestCase
+{
+    public function setUp(): void
+    {
+    }
+
+    public function testTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
